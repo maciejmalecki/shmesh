9fefb72de058671a0a9ecbaa58704467016a6445
Maciej Malecki <malecki.maciej@gmail.com>
2018-01-26 22:53:02 +0100

    Fixes

--- master/master.ino
+++ master/master.ino
@@ -12,9 +12,10 @@ RF24Mesh mesh(radio, network);
 
 void setup() {
   Serial.begin(9600);
+  Serial.println("Starting Master Unit...");
   mesh.setNodeID(0);
-  mesh.begin();
-  Serial.print("Master Unit initialized");
+    mesh.begin();
+  Serial.println("Master Unit initialized");
 }
 
 void loop()
@@ -24,12 +25,12 @@ void loop()
   while(network.available()) {
     RF24NetworkHeader header;
     char message[100];
-    network.read(header, &message, sizeof(message));
-    handleMessage(header, message);
+    uint16_t len = network.read(header, &message, sizeof(message));
+    handleMessage(header, message, len);
   }
 }
 
-void handleMessage(RF24NetworkHeader header, char *message) {
+void handleMessage(RF24NetworkHeader header, char *message, uint16_t len) {
     Serial.print("from:");
     Serial.print(header.from_node);
     Serial.print(",id:");
@@ -37,7 +38,7 @@ void handleMessage(RF24NetworkHeader header, char *message) {
     Serial.print(",type:");
     Serial.print(header.type);
     Serial.print(",message:{");
-    Serial.print(message);
-    Serial.print("}");
+    Serial.print(String(message).substring(0, len));
+    Serial.println("}");
 }
 
--- node/node.ino
+++ node/node.ino
@@ -12,18 +12,23 @@ const int ledPin = 2;
 RF24 radio(7, 8);
 RF24Network network(radio);
 RF24Mesh mesh(radio, network);
-uint8_t nodeAddress = 1;
+uint8_t nodeAddress = 4;
 
 void setup() {
-  pinMode(ledPin, OUTPUT);
   Serial.begin(9600);
+  Serial.println("Starting Node Unit...");
+  pinMode(ledPin, OUTPUT);
   mesh.setNodeID(nodeAddress);
+  Serial.print("Node Unit configured with ");
+  Serial.print(mesh.getNodeID());
+  Serial.println(" address.");
   mesh.begin();
-  Serial.print("Node Unit initialized");
+  Serial.println("Node Unit initialized");
 }
 
 void loop()
 {
+  Serial.println("wakeup");
   mesh.update();
   float temp = readTemp();
   bool error = false;
@@ -31,14 +36,14 @@ void loop()
   message.concat(temp);  
   if(!mesh.write(message.c_str(), 0x01, sizeof(message))) {
     if(!mesh.checkConnection()) {
-      Serial.print("renewing mesh address");
+      Serial.println("renewing mesh address");
       mesh.renewAddress();
     } else {
-      Serial.print("connection lost");
+      Serial.println("connection lost");
       error = true;
     }
   } else {
-    Serial.print("Data sent: " + message);  
+    Serial.println("Data sent: " + message);  
   }
   
   if (error) {
